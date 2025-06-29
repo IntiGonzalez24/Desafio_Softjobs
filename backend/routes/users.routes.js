bc46787d084fb0b333a3f961330fa17c22df7333
IntiGonzalez24 <intigonzalez24@gmail.com>
2025-06-29 00:49:47 -0400

    se realizo con exito la prueba del registro de usuarios

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -4,6 +4,6 @@ import { Router } from "express";
 
 const router = Router()
 
-router.post('/register',registerUser)
+router.post('/usuarios',registerUser)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
 import usersRoutes from './routes/users.routes.js'
+import cors from 'cors'
 
 const PORT=process.env.PORT
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use(usersRoutes)
 app.listen(PORT,console.log(`Server on http://localhost:${PORT}`))
